206c95edaea866bad9bda8b02ef600e36e885549
Gregor Tudan <gregor.tudan@cofinpro.de>
2015-08-28 13:03:25 +0200

    configure android build

--- src/main/java/de/cofinpro/dojo/minefx/Main.java
+++ src/main/java/de/cofinpro/dojo/minefx/Main.java
@@ -1,23 +1,23 @@
 package de.cofinpro.dojo.minefx;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
         primaryStage.setTitle("Minesweeper");
-        final Scene value = new Scene(new GamePanel(10,10, 10), 300, 275);
+        final Scene scene = new Scene(new GamePanel(10,10, 10), bounds.getWidth(), bounds.getHeight());
 
-        primaryStage.setScene(value);
+        primaryStage.setScene(scene);
 
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
