4c3412e0637b392ab94dd13af5b0b2019aa221d7
Gregor Tudan <gregor.tudan@cofinpro.de>
2015-08-29 10:39:00 +0200

    add client id

--- src/main/java/de/cofinpro/dojo/minefx/multiplayer/MultiplayerEvent.java
+++ src/main/java/de/cofinpro/dojo/minefx/multiplayer/MultiplayerEvent.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 
 import java.io.Serializable;
+import java.util.Random;
 
 /**
  * @author Gregor Tudan, Cofinpro AG
@@ -13,15 +14,22 @@ public abstract class MultiplayerEvent implements Serializable {
 
     public abstract void executeMove(GamePanel panel);
 
+    private static final int clientId = new Random().nextInt();
+
+    public static int getClientId() {
+        return clientId;
+    }
+
     public void execute(GamePanel panel) {
-       Task task = new Task<Void>() {
-           @Override
-           protected Void call() throws Exception {
-               executeMove(panel);
-               return null;
-           }
-       };
+        Task task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                executeMove(panel);
+                return null;
+            }
+        };
         Platform.runLater(task);
     }
+
 
 }
